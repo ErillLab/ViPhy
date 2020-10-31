569c874b157a351fbc63a2bae7d1a6b1c353cee4
Anna Folch <annafasse@gmail.com>
2020-10-31 17:20:32 +0100

    Translate DNA to protein (current version)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # This is a sample Python script.
 
+from Bio import SeqIO
+from Bio.Seq import Seq, reverse_complement
+import os.path as path
+
 
 def nucleotideToAminoacid(DNAseq):
     """
@@ -7,31 +11,10 @@ def nucleotideToAminoacid(DNAseq):
     DNAseq: nucleotide sequence, should be string
     Returns a single amino acid sequence that come from combining the six amino acid sequences, should be string
     """
-    translationTable = {
-        'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
-        'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-        'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
-        'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
-        'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
-        'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-        'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
-        'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
-        'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
-        'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
-        'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
-        'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
-        'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
-        'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-        'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
-        'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W',
-    }
 
-    proteinSeq1 = ""
-    proteinSeq2 = ""
-    proteinSeq3 = ""
-    proteinSeq4 = ""
-    proteinSeq5 = ""
-    proteinSeq6 = ""
+    protein_seq1 = ""
+    protein_seq2 = ""
+    protein_seq3 = ""
 
     # First direction
     for i in range(0, len(DNAseq), 3):
@@ -39,51 +22,44 @@ def nucleotideToAminoacid(DNAseq):
         codon2 = DNAseq[i + 1:i + 4]
         codon3 = DNAseq[i + 2:i + 5]
         if (len(codon1)==3):
-            proteinSeq1 += translationTable[codon1]
+            codon1 = Seq(codon1)
+            protein_seq1 += codon1.translate('1')
         if (len(codon2)==3):
-            proteinSeq2 += translationTable[codon2]
+            codon2 = Seq(codon2)
+            protein_seq2 += codon2.translate('1')
         if (len(codon3)==3):
-            proteinSeq3 += translationTable[codon3]
+            codon3 = Seq(codon3)
+            protein_seq3 += codon3.translate('1')
+
+    protein_seq = protein_seq1 + protein_seq2 + protein_seq3
 
     # Second direction
-    DNAseqR = revertSeq(DNAseq)
+    DNAseqR = reverse_complement(DNAseq) # Return the reverse complement sequence
+    DNAseqR = str(DNAseqR)
 
-    for j in range(0, len(DNAseqR), 3):
-        codon1 = DNAseqR[j:j + 3]
-        codon2 = DNAseqR[j + 1:j + 4]
-        codon3 = DNAseqR[j + 2:j + 5]
-        if (len(codon1) == 3):
-            proteinSeq4 += translationTable[codon1]
-        if (len(codon2) == 3):
-            proteinSeq5 += translationTable[codon2]
-        if (len(codon3) == 3):
-            proteinSeq6 += translationTable[codon3]
+    protein_seq1 = ""
+    protein_seq2 = ""
+    protein_seq3 = ""
 
-    proteinSeq = proteinSeq1 + proteinSeq2 + proteinSeq3 + proteinSeq4 + proteinSeq5 + proteinSeq6
+    for i in range(0, len(DNAseqR), 3):
+        codon1 = DNAseqR[i:i + 3]
+        codon2 = DNAseqR[i + 1:i + 4]
+        codon3 = DNAseqR[i + 2:i + 5]
+        if (len(codon1)==3):
+            codon1 = Seq(codon1)
+            protein_seq1 += codon1.translate('1')
+        if (len(codon2)==3):
+            codon2 = Seq(codon2)
+            protein_seq2 += codon2.translate('1')
+        if (len(codon3)==3):
+            codon3 = Seq(codon3)
+            protein_seq3 += codon3.translate('1')
 
-    return proteinSeq
+    protein_seq += protein_seq1 + protein_seq2 + protein_seq3
+    protein_seq = str(protein_seq)
 
-def revertSeq(DNAseq):
-    """
-    Translate a nucleotide sequence into six amino acid sequences.
-    DNAseq: nucleotide sequence, should be string
-    Returns the opposite sequence to the input sequence, should be a string
-    """
-    reversSeq= DNAseq[::-1]
-    rDNAseq = ""
-    for i in reversSeq:
-        if i == 'A':
-            rDNAseq += 'T'
-        elif i == 'C':
-            rDNAseq += 'G'
-        elif i == 'G':
-            rDNAseq += 'C'
-        elif i == 'T':
-            rDNAseq += 'A'
-        else:
-            rDNAseq += i
+    return protein_seq
 
-    return rDNAseq
 
 def fastaToString(DNAfile):
     """
@@ -96,10 +72,10 @@ def fastaToString(DNAfile):
     fastaList = fastaContent.split("\n")
     fastaName = fastaList[0]
     fastaList = fastaList[1:]
+
     DNAseq = ""
     for line in fastaList:
         DNAseq += line
-
     file.close()
     return DNAseq, fastaName
 
@@ -113,19 +89,61 @@ def exportFasta(proteinSeq, fastaName):
     file = open("proteinSeq.fasta", "w")
     for i in proteinSeq:
         fileContent += i
-        if (len(fileContent) % 63) == 0:
-            fileContent += '\n'
-    fileContent = fastaName + " " + fileContent
+        #if (len(fileContent) % 60) == 0:
+           # fileContent += '\n'
+    fileContent = fastaName + '\n' + fileContent
     file.write(fileContent)
     file.close()
+
+def compareFiles(inputFile, outputFile):
+    """
+        Compare the fasta file obtained after executing the code against the fasta file containing the expected results
+        inputFile: resulting fasta file
+        fastaName: fasta file containing the results that you hope to achieve
+        """
+    file1 = open(inputFile).readlines()
+    file1_line = ""
+
+    for lines in file1:
+        lines = lines.rstrip('\n')
+        file1_line += lines
+
+    file2 = open(outputFile).readlines()
+    file2_line = ""
+
+    for lines in file2:
+        lines = lines.rstrip('\n')
+        file2_line += lines
+
+    if file1_line == file2_line:
+        print("Match. The result is correct")
+    else:
+        if len(file1_line) != len(file2_line):
+            print("No Match. The length of both files is not the same")
+        else:
+            count = 0
+            for a, b in zip(file1_line, file2_line):
+                if a != b:
+                    count += 1
+            print("Not Match. Number of differences:", count)
 
 
 # Main function
 if __name__ == '__main__':
-    textFile = 'sequence.fasta'
-    DNAseq, fastaName = fastaToString(textFile)
+
+    fasta_file = 'sequence.fasta'
+    testFile = 'expectedResult.fasta'
+
+    DNAseq, fastaName = fastaToString(fasta_file)
     proteinSeq = nucleotideToAminoacid(DNAseq)
-    exportFasta(proteinSeq, fastaName)
+
+    exportFasta(proteinSeq, fastaName) # Fasta file containing the resulting protein sequence
+
+    if path.exists(testFile):
+        compareFiles('proteinSeq.fasta', testFile)
+
+
+
 
 
 
